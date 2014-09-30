75754d1e97348eed7495a20338a7d388286e626a
Troy G. Robertson <Troygrobertson@hotmail.com>
2014-09-30 14:35:48 -0400

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,8 @@ describe User do
 	it { should respond_to(:password_digest)}
 	it {should respond_to(:password)}
 	it { should respond_to(:password_confirmation)}
+	it { should respond_to(:remember_token)}
+	it { should respond_to(:authenticate)}
 	it {should be_valid}
 	it {should respond_to(:authenticate)}
 	describe "when name is not present" do
@@ -86,5 +88,8 @@ describe User do
 
 	end
 
-
+	describe "remember token" do
+		before{@user.save}
+		its(:remember_token){ should_not be_blank}
+	end
 end
