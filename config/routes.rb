5675ed40110bf7f5585368b2b283c53b6e024b41
Troy G. Robertson <Troygrobertson@hotmail.com>
2014-11-25 11:16:26 -0500

    removed user delete reference from admin index

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ SampleApp::Application.routes.draw do
 	#resources :users
 	resources :sessions, only: [:new, :create, :destroy]
 	root 'sessions#new'
-	match '/signup', to: 'users#new', via: 'get'
+	#match '/signup', to: 'users#new', via: 'get'
 	match '/signin', to: 'sessions#new', via: 'get'
 	match '/signout', to: 'sessions#destroy', via: 'delete'
 	match '/help', to: "static_pages#help", via: 'get'
